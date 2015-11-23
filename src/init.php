6af28f4afb0dac2ab570ea1b92349b16c9831bb3
Simon JAILLET <sjaillet@gmail.com>
2015-11-23 18:06:13 +0100

    Don't expose spec namespace && avoid multiple declaration of `box()`.

--- src/init.php
+++ src/init.php
@@ -1,7 +1,7 @@
 <?php
 use box\BoxException;
 
-if (!defined('BOX_DISABLE_FUNCTIONS') || !BOX_DISABLE_FUNCTIONS) {
+if (!function_exists('box') && (!defined('BOX_DISABLE_FUNCTIONS') || !BOX_DISABLE_FUNCTIONS)) {
 
 	function box($name, $box = null) {
 		static $boxes = [];
