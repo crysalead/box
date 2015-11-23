ce41d70230a06aa7e33c82bb318e0f703f323f46
Simon JAILLET <sjaillet@gmail.com>
2015-11-23 18:13:04 +0100

    Don't allow to expose box function twice.

--- src/init.php
+++ src/init.php
@@ -1,25 +1,36 @@
 <?php
 use box\BoxException;
 
-if (!function_exists('box') && (!defined('BOX_DISABLE_FUNCTIONS') || !BOX_DISABLE_FUNCTIONS)) {
+$defineFuctions = true;
 
-	function box($name, $box = null) {
-		static $boxes = [];
-		if ($name === false) {
-			$boxes = [];
-			return;
-		}
-		if ($box === false) {
-			unset($boxes[$name]);
-			return;
-		}
-		if ($box) {
-			return $boxes[$name] = $box;
-		}
-		if (isset($boxes[$name])) {
-			return $boxes[$name];
-		}
-		throw new BoxException("Unexisting box `'{$name}'`.");
-	}
+if (getenv('BOX_DISABLE_FUNCTIONS') || (defined('BOX_DISABLE_FUNCTIONS') && BOX_DISABLE_FUNCTIONS)) {
+    $defineFuctions = false;
+}
+
+if (defined('BOX_FUNCTIONS_EXIST') && BOX_FUNCTIONS_EXIST) {
+    $defineFuctions = false;
+}
+
+if ($defineFuctions) {
+    define('BOX_FUNCTIONS_EXIST', true);
+
+    function box($name, $box = null) {
+        static $boxes = [];
+        if ($name === false) {
+            $boxes = [];
+            return;
+        }
+        if ($box === false) {
+            unset($boxes[$name]);
+            return;
+        }
+        if ($box) {
+            return $boxes[$name] = $box;
+        }
+        if (isset($boxes[$name])) {
+            return $boxes[$name];
+        }
+        throw new BoxException("Unexisting box `'{$name}'`.");
+    }
 
 }
